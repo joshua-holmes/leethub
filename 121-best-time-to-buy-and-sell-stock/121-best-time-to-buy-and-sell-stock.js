5d2bf66294d1f1a3f1e25aad59a84e07fca94fc3
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2022-02-28 13:53:37 -0600

    Time: 110 ms (65.81%), Space: 51.6 MB (46.59%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,17 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    if (prices.length === 1) return 0;
-    
-    let p = prices[0];
-    let q = prices[1];
-    let max = 0;
-    
-    for (let i = 1; i < prices.length; i++) {
-        max = Math.max(max, q-p);
-        if (p > q) p = q
-        q = prices[i + 1]; // make q the next iteration
-    }
-    
-    return max;
+    let min = prices[0]
+    let maxProf = 0;
+    prices.forEach(p => {
+        if (p < min) min = p;
+        maxProf = Math.max(maxProf, p - min);
+    })
+    return maxProf;
 };
