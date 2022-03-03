a6a6d409f478158073eae2da0372c56328b4ca2e
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2022-03-03 16:23:05 -0600

    Time: 56 ms (97.53%), Space: 42.3 MB (5.67%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,17 +3,10 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    const fib = [1, 2]
+    const fib = [1, 2];
     if (n === 1) return 1;
     for (let i = 2; i < n; i++) {
         fib.push(fib[i-2] + fib[i-1]);
     }
-    return fib[fib.length - 1]
+    return fib[fib.length - 1];
 };
-
-// 2 => 2
-// 3 => 3
-// 4 => 5
-// 5 => 8
-// 6 => 13
-// 7 => 21
