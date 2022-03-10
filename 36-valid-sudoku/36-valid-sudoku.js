7c814666c92d80c2d03701a9eaac616b6cd48ed6
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2022-03-10 17:24:23 -0600

    Time: 93 ms (76.38%), Space: 45.1 MB (51.96%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -0,0 +1,38 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    function findQuadrant(x, y) {
+      return Math.floor(y / 3) * 3 + Math.floor(x / 3);
+    }
+    const columns = [[],[],[],[],[],[],[],[],[]];
+    const box = [[],[],[],[],[],[],[],[],[]];
+    // loop row
+    for (let row = 0; row < 9; row++) {
+        // loop column
+        for (let col = 0; col < 9; col++) {
+            if (board[row][col] !== '.') {
+                
+                // check column for dup
+                if (columns[col].includes(board[row][col])) return false;
+                
+                // add value to translated board
+                columns[col].push(board[row][col]);
+                
+                // check row for dup
+                const lastInd = board[row].lastIndexOf(board[row][col])
+                if (lastInd !== col) return false;
+                
+                // set quadrant
+                const quadrant = findQuadrant(col, row);
+                // check box
+                if (box[quadrant].includes(board[row][col])) return false;
+                
+                // add value to box
+                box[quadrant].push(board[row][col]);
+            }
+        }
+    }
+    return true;
+};
