a2843ad3d7875099b86b8a73f71bc46e79626c7f
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-09-27 16:49:44 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (34.01%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.rs
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
         let max_candies = *candies.iter().max().unwrap();
-        let mut answer = Vec::new();
+        let mut answer = Vec::with_capacity(candies.len());
         
         for candy in candies {
             answer.push(candy + extra_candies >= max_candies);
