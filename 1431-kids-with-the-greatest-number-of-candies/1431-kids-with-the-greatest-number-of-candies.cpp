e96b7ca7bc804bf335804629d8dacc11e218560d
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-09-27 16:33:40 -0700

    Time: 2 ms (52.69%), Space: 9.4 MB (5.73%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,15 +1,23 @@
-#include <bits/stdc++.h>
-
+int* maxElement(vector<int>::iterator begin, vector<int>::iterator end) {
+    int* maxNum = NULL;
+    for (vector<int>::iterator itr = begin; itr < end; itr++) {
+        if (!maxNum || *maxNum < *itr)
+            maxNum = &*itr;
+    }
+    
+    return maxNum;
+}
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         vector<bool> answer(candies.size());
-        int maxCandies = *std::max_element(candies.begin(), candies.end());
+        int* maxCandiesPtr = maxElement(candies.begin(), candies.end());
         
         for (int i = 0; i < candies.size(); i++) {
-            answer[i] = candies[i] + extraCandies >= maxCandies;
+            answer[i] = candies[i] + extraCandies >= *maxCandiesPtr;
         }
         
         return answer;
     }
 };
+
