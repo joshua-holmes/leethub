d5d80306f5450bd0a9ed9178793451fbb4e0decc
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-09-28 18:10:44 -0700

    Time: 16 ms (28.86%), Space: 20.8 MB (6.19%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,17 +4,10 @@ public:
         // 0 = empty pot
         // 1 = full pot
         // 2 = empty, but is viable to be potted
-        int prev = -1;
+        int prev = 0;
         int total_pottable = 0;
         for (auto pot : flowerbed) {
             switch (prev) {
-                case -1:
-                    if (pot == 0) {
-                        prev = 2;
-                        total_pottable += 1;
-                        continue;
-                    }
-                    break;
                 case 0:
                     if (pot == 0) {
                         prev = 2;
