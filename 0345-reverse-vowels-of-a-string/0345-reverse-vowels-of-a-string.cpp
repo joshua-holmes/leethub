dc71a3a2599347fc1a78e7a8d648ae115bceab67
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-09-29 18:45:22 -0700

    Time: 8 ms (43.85%), Space: 8 MB (41.58%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,22 +2,25 @@
 
 class Solution {
 public:
-    bool isVowel(char* letter) {
-        string vowels = "aeiouAEIOU";
-        for (char v : vowels) {
-            if (*letter == v)
-                return true;
-        }
-
-        return false;
-    }
     string reverseVowels(string s) {
         int left = 0;
         int right = s.size() - 1;
         
         while (left < right) {
-            bool leftIsVowel = isVowel(&s[left]);
-            bool rightIsVowel = isVowel(&s[right]);
+            bool leftIsVowel = false;
+            bool rightIsVowel = false;
+            string vowels = "aeiouAEIOU";
+            for (char v : vowels) {
+                if (s[left] == v) {
+                    leftIsVowel = true;
+                }
+                if (s[right] == v) {
+                    rightIsVowel = true;
+                }
+                if (leftIsVowel && rightIsVowel) {
+                    break;
+                }
+            }
             if (!leftIsVowel)
                 left++;
             if (!rightIsVowel)
