31133007211f6bf8d0473333c57464c1d7e02169
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-02 16:25:08 -0700

    Time: 10 ms (59.74%), Space: 3.1 MB (86.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
@@ -1,15 +1,18 @@
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut answer = Vec::with_capacity(nums.len());
-        answer.push(1);
-        for i in 1..nums.len() {
-            answer.push(nums[i - 1] * answer[i - 1]);
-        }
+        let mut answer = vec![1; nums.len()];
+        
+        let mut left = 1;
         let mut right = 1;
-        for i in (0..nums.len()).rev() {
-            answer[i] *= right;
-            right *= nums[i];
+        
+        let len = nums.len();
+        for i in 0..len {
+            answer[i] *= left;
+            answer[len - 1 - i] *= right;
+            left *= nums[i];
+            right *= nums[len -1 - i];
         }
+        
         answer
     }
 }
