4b1cc1dc32f131e0748e66d6e9aada8a4ee172a6
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-03 21:11:49 -0700

    Time: 9 ms (88.12%), Space: 5.1 MB (26.73%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.rs
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.rs
@@ -1,27 +1,34 @@
 impl Solution {
     pub fn increasing_triplet(nums: Vec<i32>) -> bool {
-        let mut min_num: Option<i32> = None;
-        let mut target: Option<i32> = None;
+//         let mut min_num: Option<i32> = None;
+//         let mut target: Option<i32> = None;
 
+//         for n in nums {
+//             if let Some(t) = target {
+//                 if n >= t { return true; }
+//             }
+//             match min_num {
+//                 Some(min) => {
+//                     if n < min {
+//                         min_num = Some(n);
+//                     } else if n > min && n != i32::MAX {
+//                         target = match target {
+//                             Some(t) => Some(std::cmp::min(t, n + 1)),
+//                             None => Some(n + 1)
+//                         };
+//                     }
+//                 }
+//                 None => { min_num = Some(n) }
+//             }
+//         }
+
+//         false
+        let (mut a, mut b) = (i32::MAX, i32::MAX);
         for n in nums {
-            if let Some(t) = target {
-                if n >= t { return true; }
-            }
-            match min_num {
-                Some(min) => {
-                    if n < min {
-                        min_num = Some(n);
-                    } else if n > min && n != i32::MAX {
-                        target = match target {
-                            Some(t) => Some(std::cmp::min(t, n + 1)),
-                            None => Some(n + 1)
-                        };
-                    }
-                }
-                None => { min_num = Some(n) }
-            }
+            if n <= a { a = n }
+            else if n <= b { b = n }
+            else { return true; }
         }
-
-        false
+        return false;
     }
 }
