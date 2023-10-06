05282c0f18b594e70b01d47e8eec6261620ee1d0
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-05 22:54:16 -0700

    Time: 3 ms (98.31%), Space: 2.2 MB (46.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.rs
+++ 0283-move-zeroes/0283-move-zeroes.rs
@@ -1,23 +1,13 @@
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
-        let mut left: usize = 0; // used for finding 0s
-        let mut right: usize = 0; // used for finding non-0s
-        while right < nums.len() {
-            while nums[right] == 0 {
-                right += 1;
-                if right >= nums.len() { return; }
+        let mut snowBallSize = 0;
+        for i in 0..nums.len() {
+            if nums[i] == 0 {
+                snowBallSize += 1;
+            } else if snowBallSize > 0 {
+                nums[i - snowBallSize] = nums[i];
+                nums[i] = 0;
             }
-            while nums[left] != 0 {
-                left += 1;
-                if left >= nums.len() { return; }
-            }
-            while left > right || nums[right] == 0 {
-                right += 1;
-                if right >= nums.len() { return; }
-            }
-            
-            nums[left] = nums[right];
-            nums[right] = 0;
         }
     }
 }
