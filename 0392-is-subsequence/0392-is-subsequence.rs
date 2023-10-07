1743b6a3f2d200a11cb501308f851524ce3b4181
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-07 14:42:20 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (69.91%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.rs
+++ 0392-is-subsequence/0392-is-subsequence.rs
@@ -6,22 +6,20 @@ impl Solution {
         
         let mut s_chars = s.chars();
         let mut t_chars = t.chars();
-        let mut cur_s = s_chars.next();
-        let mut cur_t = t_chars.next();
+        let mut cur_s: Option<char> = s_chars.next();
+        let mut cur_t: Option<char> = t_chars.next();
         loop {
-            match cur_s {
-                Some(s) => {
-                    match cur_t {
-                        Some(t) => {
-                            if t == s {
-                                cur_s = s_chars.next();
-                            }
-                            cur_t = t_chars.next();
-                        }
-                        None => return false
+            if let Some(s) = cur_s {
+                if let Some(t) = cur_t {
+                    if t == s {
+                        cur_s = s_chars.next();
                     }
+                    cur_t = t_chars.next();
+                } else {
+                    return false;
                 }
-                None => return true
+            } else {
+                return true;
             }
         }
     }
