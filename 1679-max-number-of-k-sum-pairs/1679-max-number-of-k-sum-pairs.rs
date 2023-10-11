faf01ad1a8ad40bf85aa4002c4e33f851759974f
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-10 21:40:52 -0700

    Time: 40 ms (7.09%), Space: 3.7 MB (14.89%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.rs
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.rs
@@ -1,24 +1,28 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
         let mut nums = nums.clone();
         nums.sort();
-        let mut start = 0;
-        let mut end = nums.len() - 1;
-        let mut counter = 0;
+        let mut h_map = HashMap::new();
+        let mut count = 0;
         
-        while start < end {
-
-            if nums[start] + nums[end] < k {
-                start += 1;
-            } else if nums[start] + nums[end] > k {
-                end -= 1;
+        for n in nums {
+            let target = k - n;
+            if let Some(target_count) = h_map.get_mut(&target) {
+                if *target_count > 0 {
+                    count += 1;
+                    if *target_count == 1 {
+                        h_map.remove(&target);
+                    } else {
+                        *target_count -= 1;
+                    }
+                }
             } else {
-                counter += 1;
-                start += 1;
-                end -= 1;
+                h_map.entry(n).and_modify(|t_count| *t_count += 1).or_insert(1);
             }
         }
         
-        counter
+        count
     }
 }
