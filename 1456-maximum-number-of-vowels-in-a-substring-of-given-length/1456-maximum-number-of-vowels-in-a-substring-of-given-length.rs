157035f184799898d56d454188f8cbd37dc6e6dd
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-12 16:30:44 -0700

    Time: 2 ms (98.37%), Space: 2.2 MB (87.80%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.rs
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.rs
@@ -1,13 +1,12 @@
 use std::cmp;
-use std::collections::HashSet;
 
 impl Solution {
     pub fn max_vowels(s: String, k: i32) -> i32 {
         let mut max = 0;
         let string = s.as_bytes();
         
-        // store vowels in set for O(1) lookup time
-        let vowels = HashSet::from(['a', 'e', 'i', 'o', 'u']);
+        // using array because for some reason, it is 4x faster than using HashSet
+        let vowels = ['a', 'e', 'i', 'o', 'u'];
         
         // count of vowels as window (i through j) progresses
         let mut cur_count = 0;
@@ -18,11 +17,11 @@ impl Solution {
             if i == 0 {
                 // first, count how many vowels from i to j
                 cur_count = string[i..=j].iter().fold(0,
-                    |a, e| if vowels.get(&(*e as char)).is_some() { a + 1 } else { a }
+                    |a, e| if vowels.contains(&(*e as char)) { a + 1 } else { a }
                 );
             } else {
                 // every time after, only check last char of window and add to cur_count if vowel
-                if vowels.get(&(string[j] as char)).is_some() {
+                if vowels.contains(&(string[j] as char)) {
                     cur_count += 1;
                 }
             }
@@ -30,7 +29,7 @@ impl Solution {
             max = cmp::max(max, cur_count);
             
             // only check first char and decrement cur_count if vowel
-            if vowels.get(&(string[i] as char)).is_some() {
+            if vowels.contains(&(string[i] as char)) {
                 cur_count -= 1;
             }
         }
