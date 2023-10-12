bcfaa2d8bec90fab9dd9eb898c99aaf03208cfb3
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-11 20:25:48 -0700

    Time: 232 ms (34.74%), Space: 3 MB (28.42%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.rs
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
         let mut max = i32::MIN;
-        if nums.len() == 1 { return nums[0] as f64; }
+        // if nums.len() == 1 { return nums[0] as f64; }
         
         for left in 0..(nums.len() - k as usize + 1) {
             let right = left + k as usize - 1;
