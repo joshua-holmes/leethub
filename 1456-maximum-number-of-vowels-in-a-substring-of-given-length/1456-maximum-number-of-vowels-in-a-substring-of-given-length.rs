c5e1e130f03addf0a082a9d3b5bb044ee3c7056b
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-12 16:13:36 -0700

    Time: 5 ms (76.42%), Space: 2.3 MB (47.97%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.rs
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.rs
@@ -5,21 +5,28 @@ impl Solution {
         let mut max = 0;
         let string = s.as_bytes();
         let vowels = ['a', 'e', 'i', 'o', 'u'];
+        
+        // count of vowels as window (i through j) progresses
         let mut cur_count = 0;
         
         for i in 0..(s.len() - k as usize + 1) {
             let j = i + k as usize - 1;
+            
             if i == 0 {
+                // first, count how many vowels from i to j
                 cur_count = string[i..=j].iter().fold(0,
                     |a, e| if vowels.contains(&(*e as char)) { a + 1 } else { a }
                 );
             } else {
+                // every time after, only check last char of window and add to cur_count if vowel
                 if vowels.contains(&(string[j] as char)) {
                     cur_count += 1;
                 }
-            }            
+            }
+            
             max = cmp::max(max, cur_count);
             
+            // only check first char and decrement cur_count if vowel
             if vowels.contains(&(string[i] as char)) {
                 cur_count -= 1;
             }
