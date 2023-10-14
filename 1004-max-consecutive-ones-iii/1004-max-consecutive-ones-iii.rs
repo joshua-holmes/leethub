5f5bef2fdde7c0da00279c47124f814c0542fb22
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-13 19:14:31 -0700

    Time: 7 ms (78.16%), Space: 2.2 MB (66.67%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.rs
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.rs
@@ -2,41 +2,23 @@ use std::cmp;
 
 impl Solution {
     pub fn longest_ones(nums: Vec<i32>, k: i32) -> i32 {
-        let mut max = 0;
-        let mut cur_1_count = 0;
         let mut left = 0;
         let mut right = 0;
         let mut my_k = k;
         
         while right < nums.len() {
-            if nums[right] == 1 || my_k > 0 {
-                // move `right`
-
-                if nums[right] == 0 {
-                    my_k -= 1;
+            if (nums[right] == 0) {
+                my_k -= 1;
+            }
+            if (my_k < 0) {
+                if nums[left] == 0 {
+                    my_k += 1;
                 }
-
-                cur_1_count += 1;
-                max = cmp::max(max, cur_1_count);
-                right += 1;
-            } else {
-                // move `left`
-
-                if nums[left] == 1 || my_k < k  {
-                    if nums[left] == 0 {
-                        my_k += 1;
-                    }
-                    cur_1_count -= 1;
-                }
-                
-                if left == right {
-                    right += 1;
-                }
-                
                 left += 1;
             }
+            right += 1;
         }
         
-        max
+        (right - left) as i32
     }
 }
