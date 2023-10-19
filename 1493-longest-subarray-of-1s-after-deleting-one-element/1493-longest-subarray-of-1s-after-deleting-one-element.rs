94ab00c7b69ec629f93bfa1e1f2f9109d1c002a1
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-18 21:04:19 -0700

    Time: 6 ms (28.57%), Space: 2.5 MB (61.22%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.rs
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.rs
@@ -1,25 +1,21 @@
 impl Solution {
     pub fn longest_subarray(nums: Vec<i32>) -> i32 {
         let mut num_of_0s = 0;
-        let mut left: i32 = 0;
+        let mut left = 0;
         let mut max = 0;
         
         for (i, n) in nums.iter().enumerate() {
-            println!("{:?} {:?}", left, i);
             if *n == 0 {
                 num_of_0s += 1;
             }
             while num_of_0s > 1 {
-                if left > -1 && nums[left as usize] == 0 {
+                if nums[left] == 0 {
                     num_of_0s -= 1;
                 }
                 left += 1;
             }
-            
-            
-            println!("{:?}", num_of_0s);
             if num_of_0s <= 1 {
-                let count = i as i32 - left;
+                let count = (i - left) as i32;
                 if count > max { max = count; }
             }
         }
