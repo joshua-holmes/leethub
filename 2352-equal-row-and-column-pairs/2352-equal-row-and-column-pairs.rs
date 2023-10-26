7aa1c8bad73c59abb09ae73aded3afca530cf16a
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-10-25 20:18:19 -0700

    Time: 6 ms (91.67%), Space: 2.9 MB (45.00%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.rs
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.rs
@@ -1,20 +1,23 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn equal_pairs(grid: Vec<Vec<i32>>) -> i32 {
         let mut count = 0;
         let len = grid.len();
         
-        let mut cols: Vec<Vec<i32>> = vec![Vec::with_capacity(len); len];
+        let mut rows = HashMap::with_capacity(len);
         for row in &grid {
-            for i in 0..len {
-                cols[i].push(row[i]);
-            }
+            *rows.entry(row).or_insert(0) += 1;
         }
         
         for i in 0..len {
+            let mut col = Vec::with_capacity(len);
             for j in 0..len {
-                if cols[i] == grid[j] {
-                    count += 1;
-                }
+                col.push(grid[j][i]);
+            }
+            
+            if let Some(value) = rows.get(&col) {
+                count += value;
             }
         }
         
