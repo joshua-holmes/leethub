4087c4f0b8bbd8c64d25a0cc635e8d105c4b6818
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2023-11-27 18:14:52 -0800

    Time: 2 ms (76.79%), Space: 2.3 MB (17.86%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn asteroid_collision(asteroids: Vec<i32>) -> Vec<i32> {
+        let mut new_astroids = Vec::with_capacity(asteroids.len());
+        
+        'main: for a in asteroids {
+            if a > 0 {
+                new_astroids.push(a);
+                continue;
+            }
+            
+            while let Some(new_a) = new_astroids.last() {
+                if *new_a > 0 {
+                    if -a > *new_a {
+                        new_astroids.pop();
+                    } else if -a < *new_a {
+                        continue 'main;
+                    } else {
+                        new_astroids.pop();
+                        continue 'main;
+                    }
+                } else {
+                    break;
+                }
+            }
+            
+            new_astroids.push(a);
+        }
+        
+        new_astroids
+    }
+}
