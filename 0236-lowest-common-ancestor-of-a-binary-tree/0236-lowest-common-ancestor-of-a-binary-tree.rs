472ea06c950121954de90edba36d9697a925666f
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2024-01-16 15:55:15 -0800

    Time: 4 ms (64.06%), Space: 4.5 MB (42.19%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.rs
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.rs
@@ -21,35 +21,46 @@ use std::cell::RefCell;
 impl Solution {
     pub fn lowest_common_ancestor(root: Option<Rc<RefCell<TreeNode>>>, p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
         let root = root.unwrap();
-        let mut lca = None;
+        let lca = None;
         let p_val = p.unwrap().try_borrow().unwrap().val;
         let q_val = q.unwrap().try_borrow().unwrap().val;
         
-        Self::dfs(root.clone(), &mut lca, p_val, q_val);
+        let lca = Self::dfs(root.clone(), lca, p_val, q_val).2;
         
         lca
     }
     
-    fn dfs(node_rc: Rc<RefCell<TreeNode>>, lca: &mut Option<Rc<RefCell<TreeNode>>>, p_val: i32, q_val: i32) -> (bool, bool) {
+    fn dfs(
+        node_rc: Rc<RefCell<TreeNode>>,
+        mut lca: Option<Rc<RefCell<TreeNode>>>,
+        p_val: i32,
+        q_val: i32
+    ) -> (
+        bool, // has_p
+        bool, // has_q
+        Option<Rc<RefCell<TreeNode>>> // lca
+    ) {
         let node = node_rc.try_borrow().unwrap();
         
         let mut has_p = node.val == p_val;
         let mut has_q = node.val == q_val;
-        if let Some(n) = node.left.as_ref() {
+        let lca = if let Some(n) = node.left.as_ref() {
             let results = Self::dfs(n.clone(), lca, p_val, q_val);
             has_p = has_p || results.0;
             has_q = has_q || results.1;
-        };
-        if let Some(n) = node.right.as_ref() {
+            results.2
+        } else { lca };
+        let mut lca = if let Some(n) = node.right.as_ref() {
             let results = Self::dfs(n.clone(), lca, p_val, q_val);
             has_p = has_p || results.0;
             has_q = has_q || results.1;
-        }
+            results.2
+        } else { lca };
         
         if lca.is_none() && has_p && has_q {
-            *lca = Some(node_rc.clone());
+            lca = Some(node_rc.clone());
         }
         
-        (has_p, has_q)
+        (has_p, has_q, lca)
     }
 }
