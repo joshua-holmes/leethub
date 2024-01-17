0ed1c3d494912f48eb39916fc0d9428ac6418ef1
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2024-01-16 16:03:36 -0800

    Time: 0 ms (100.00%), Space: 4.6 MB (35.94%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.rs
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.rs
@@ -25,6 +25,8 @@ impl Solution {
         let p_val = p.unwrap().try_borrow().unwrap().val;
         let q_val = q.unwrap().try_borrow().unwrap().val;
         
+        // lca's ownership gets passed through the tree
+        // this is an alternative option to mutable borrowing
         let lca = Self::dfs(root.clone(), lca, p_val, q_val).2;
         
         lca
@@ -42,21 +44,27 @@ impl Solution {
     ) {
         let node = node_rc.try_borrow().unwrap();
         
+        // this is `true` if this node is p or q
         let mut has_p = node.val == p_val;
         let mut has_q = node.val == q_val;
+        
+        // send lca through the tree to get set as `Some(_)` if it finds both p and q
         let lca = if let Some(n) = node.left.as_ref() {
             let results = Self::dfs(n.clone(), lca, p_val, q_val);
             has_p = has_p || results.0;
             has_q = has_q || results.1;
-            results.2
+            results.2 // returning lca's ownership
         } else { lca };
         let mut lca = if let Some(n) = node.right.as_ref() {
             let results = Self::dfs(n.clone(), lca, p_val, q_val);
             has_p = has_p || results.0;
             has_q = has_q || results.1;
-            results.2
+            results.2 // returning lca's ownership
         } else { lca };
         
+        // only set lca if it is not already set
+        // since this occurs after the recursion, this will get set first in the deepest part of the tree
+        // since we only set lca if it is `None`, we do not overwrite lca with higher nodes after it has been written to
         if lca.is_none() && has_p && has_q {
             lca = Some(node_rc.clone());
         }
