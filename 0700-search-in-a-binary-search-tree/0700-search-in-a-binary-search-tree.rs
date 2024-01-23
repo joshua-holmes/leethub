79d2163228b158d6635360ec5469e3c9a2c70bf4
Joshua Holmes <91363480+joshua-holmes@users.noreply.github.com>
2024-01-22 21:30:57 -0800

    Time: 2 ms (89.06%), Space: 2.7 MB (67.19%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.rs
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.rs
@@ -20,15 +20,20 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn search_bst(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
-        let mut node = root;
-        while let Some(n) = node {
-            let node_val = n.try_borrow().unwrap().val;
+        let mut cur_node = root;
+        while let Some(node_rc) = cur_node {
+            // check if this node is the correct node
+            let node_val = node_rc.try_borrow().unwrap().val;
             if node_val == val {
-                return Some(n);
-            } else if node_val > val {
-                node = n.try_borrow().unwrap().left.clone();
+                return Some(node_rc);
+            }
+            
+            // search right and left if not
+            let node = node_rc.try_borrow().unwrap();
+            if node_val > val {
+                cur_node = node.left.clone();
             } else {
-                node = n.try_borrow().unwrap().right.clone();
+                cur_node = node.right.clone();
             }
         }
         
